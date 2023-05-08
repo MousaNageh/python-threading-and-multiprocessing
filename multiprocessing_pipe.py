46ba82e9214c8d82299bd82eed30195c40a16d59
mousanageh <200moussa200@gmail.com>
2023-05-08 21:53:34 +0300

    update

--- multiprocessing_pipe.py
+++ multiprocessing_pipe.py
@@ -1,23 +1,47 @@
 import multiprocessing
-
-def child_process(conn):
-    # Receive a message from the parent process
+import time 
+def proccess1(conn):
     message = conn.recv()
-    print(f"Child process received message: {message}")
-    # Send a response to the parent process
-    response = "Hello from the child process!"
-    conn.send(response)
-    # Close the connection
+    print(message)
+    conn.send("hello process 2 ")
     conn.close()
+    return "procees 1 done"
 
-# Create a pipe for communication between the parent and child processes
-parent_conn, child_conn = multiprocessing.Pipe()
-# Spawn a child process to run the child_process function
-p = multiprocessing.Process(target=child_process, args=(child_conn,))
-p.start()
-# Send a message to the child process
-message = "Hello from the parent process!"
-parent_conn.send(message)
-# Receive a response from the child process
-response = parent_conn.recv()
-print(f"Parent process received response: {response}")
+def proccess2(conn):
+    conn.send("hello proccess 1")
+    message = conn.recv()
+    print(message)
+    conn.close()
+    return "procees 2 done"
+conn1, conn2 = multiprocessing.Pipe()
+
+#2 process 
+p1 = multiprocessing.Process(target=proccess1, args=(conn1,))
+p2 = multiprocessing.Process(target=proccess2, args=(conn2,))
+p1.start()
+p2.start()
+
+
+
+
+# with process pool 
+from concurrent.futures import ProcessPoolExecutor ,as_completed
+with ProcessPoolExecutor() as executor:
+    future_objs = [executor.submit(proccess1,conn1),executor.submit(proccess2,conn2)] 
+    for res in as_completed(future_objs):
+        print(res.result())
+
+
+
+
+
+# for one procese 
+p1 = multiprocessing.Process(target=proccess1, args=(conn1,))
+p1.start()
+# wait conn1 to send value 
+
+conn2.send("hello from main process")
+p1_res = conn2.recv()
+print(p1_res)
+conn2.close()
+
